208ed47dd6931e9d9d0f02ce791f7892adc580e2
fathimahiba0803-crypto <fathimahiba0803@gmail.com>
2026-02-07 20:37:23 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+class Calculator {
+
+    // Method to add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method to multiply two integers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Method to square an integer
+    public int square(int a) {
+        return a * a;
+    }
+}
